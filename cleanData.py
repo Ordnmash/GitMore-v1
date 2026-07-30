772f4eaee40b2c1db359c39e07742c097608bfd4
Ordnmash <ordnfxist@gmail.com>
2026-07-30 11:05:31 +0200

    Add function to remove specific symbols from commits
    Implement function to clean unwanted commit messages from commits.txt.

---
+++ cleanData.py
@@ -0,0 +1,10 @@
+# this code is used to clean the commits.txt to remove unecessary commits messages or non-english commits
+def remove_fromData(symbol):
+  for _ in range(100):
+    for i,c in commits:
+      if symbol in c:
+        commits.pop(i)
+        break # this break is to reset the indexing and so we don't pop wrong commits strings
+
+remove_fromData('$')
+# then all commits containing '$' are removed if less than 100 commits, you might need to re-call the function if there are commits containing the unwanted symbols
