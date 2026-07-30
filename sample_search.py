e8b57f52a23c9132f6aa9dd1d109afe550fd4cdd
Ordnmash <ordnfxist@gmail.com>
2026-07-30 11:12:21 +0200

    Implement sample and search functions in sample_search.py
    Added sample function for model interaction and search function for commit matching.

---
+++ sample_search.py
@@ -0,0 +1,27 @@
+def sample(nums): # this function is used to ask the model to generate the commits like, it works well
+  for i in range(nums):
+    out = []
+    context = [0] * block_size
+    while True:
+      contx  = torch.tensor([context])
+      logits = model(contx)
+      probs  = F.softmax(logits, dim=1)
+      ix     = torch.multinomial(probs, num_samples=1, replacement=True).item()
+      if ix == 0:
+        break
+      else:
+        out.append(itos[ix])
+      context = context[1:] + [ix]
+    print(''.join(out))
+    print("\n")
+    
+# to search whether the model is getting some words type from the data or it's just making them up
+def search(x: str):
+  count= 0
+  cont = []
+  for c in commits:
+    if x.lower() in c.lower():
+      cont.append(c)
+      count+=1
+  print(f'{count} matches found...')
+  return cont if cont else None
