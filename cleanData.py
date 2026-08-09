653b735fb4a1c87a61f6284e0801d72a717594c6
Ordnmash <ordnfxist@gmail.com>
2026-08-10 00:23:52 +0200

    Change symbol to remove from data in cleanData.py

--- cleanData.py
+++ cleanData.py
@@ -5,5 +5,5 @@ def remove_fromData(symbol):
       commits.pop(i) # note that data would change indexing positions as you pop.
       i-=1 # this aligns back the indexig position for i && c to align again.
 
-remove_fromData('$')
-# then all commits containing '$' are removed if less than 100 commits, you might need to re-call the function if there are commits containing the unwanted symbols
+remove_fromData('~')
+# then all commits containing '~' are removed if less than 100 commits, you might need to re-call the function if there are commits containing the unwanted symbols
