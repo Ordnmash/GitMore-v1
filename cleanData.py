5c61bd57865bb531fd1010e248b56fa89b68c705
Ordnmash <ordnfxist@gmail.com>
2026-08-09 23:37:48 +0200

    Improve remove_fromData function logic
    Refactor remove_fromData function to iterate through commits without a fixed range.

--- cleanData.py
+++ cleanData.py
@@ -1,9 +1,8 @@
 # this code is used to clean the commits.txt to remove unecessary commits messages or non-english commits
 def remove_fromData(symbol):
-  for _ in range(100):
-    for i,c in commits:
-      if symbol in c:
-        commits.pop(i)
+  for i,c in commits:
+    if symbol in c:
+      commits.pop(i)
 
 remove_fromData('$')
 # then all commits containing '$' are removed if less than 100 commits, you might need to re-call the function if there are commits containing the unwanted symbols
