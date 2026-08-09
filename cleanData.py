f381d7f1ec15a26ddec8e804e7d4eda79a8ca520
Ordnmash <ordnfxist@gmail.com>
2026-08-09 23:39:57 +0200

    Fix indexing issue in remove_fromData function
    Adjust index after popping elements from commits list to maintain alignment.

--- cleanData.py
+++ cleanData.py
@@ -3,6 +3,7 @@ def remove_fromData(symbol):
   for i,c in commits:
     if symbol in c:
       commits.pop(i) # note that data would change indexing positions as you pop.
+      i-=1 # this aligns back the indexig position for i && c to align again.
 
 remove_fromData('$')
 # then all commits containing '$' are removed if less than 100 commits, you might need to re-call the function if there are commits containing the unwanted symbols
