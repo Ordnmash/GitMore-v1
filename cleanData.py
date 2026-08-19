8efc439ad0448147b84c3fc8347cb1c00a200563
Ordnmash <ordnfxist@gmail.com>
2026-08-19 22:11:25 +0200

    Refactor remove_fromData function in cleanData.py
    Fix indentation and comment formatting in remove_fromData function.

--- cleanData.py
+++ cleanData.py
@@ -1,6 +1,5 @@
 # this code is used to clean the commits.txt to remove unecessary commits messages or non-english commits
 def remove_fromData(symbol):
-
   for i,c in commits:
     if symbol in c:
       commits.pop(i) # note that data would change indexing positions as you pop.
