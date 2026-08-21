996c74182cb9844a389cdfb4e80fa14f5a9fc23b
Ordnmash <ordnfxist@gmail.com>
2026-08-21 22:38:23 +0200

    Change softmax dimension from 1 to -1

--- sample_search.py
+++ sample_search.py
@@ -5,7 +5,7 @@ def sample(nums): # this function is used to ask the model to generate the commi
     while True:
       contx  = torch.tensor([context])
       logits = model(contx)
-      probs  = F.softmax(logits, dim=1)
+      probs  = F.softmax(logits, dim=-1)
       ix     = torch.multinomial(probs, num_samples=1, replacement=True).item()
     
       if ix == 0: # because 0 is the special ENDtoken we want to break assuming the model predicts the sentence comes to an end.
