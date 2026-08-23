f8f252d3e4b45555fe1822823a06b0d898efc861
Ordnmash <ordnfxist@gmail.com>
2026-08-23 22:29:59 +0200

    Add type hint for symbol parameter in remove_fromData

--- cleanData.py
+++ cleanData.py
@@ -1,5 +1,5 @@
 # this code is used to clean the commits.txt to remove unecessary commits messages or non-english commits
-def remove_fromData(symbol):
+def remove_fromData(symbol: str):
   for i,c in commits:
     if symbol in c:
       commits.pop(i)
