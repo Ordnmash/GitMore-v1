0fef9b55f6bd3cd66eebd43523f05aad2e5f6c42
ordnmash <ordnfxist@gmail.com>
2026-09-05 23:35:11 +0200

    Refactor remove_fromData function
    Refactor remove_fromData function to clarify intent and fix return statement.

--- cleanData.py
+++ cleanData.py
@@ -1,9 +1,7 @@
-# this code is used to clean the commits.txt to remove unecessary commits messages or non-english commits
 def remove_fromData(symbol: str):
-  for i,c in commits:
+  for i,c in commits: # are commits tuples?
     if symbol in c:
       commits.pop(i)
       i-=1
-  return #None
-
+  return None
 remove_fromData('~')
