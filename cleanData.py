53bf63696e3311dbd804e418e8481df7dd0d49ff
ordnmash <ordnfxist@gmail.com>
2026-09-11 22:49:58 +0200

    Call remove_fromData function with tilde symbol

--- cleanData.py
+++ cleanData.py
@@ -1,7 +1,10 @@
+# commits = [str,....str]
+
 def remove_fromData(symbol: str):
   for i,c in enumerate(commits):
     if symbol in c:
       commits.pop(i)
       i-=1
   return None
+  
 remove_fromData('~')
