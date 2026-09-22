b1c0c302ee66e6394b62ee3e3c1bc8cc8869b94c
ordnmash <ordnfxist@gmail.com>
2026-09-22 22:58:38 +0200

    Improve formatting in sample and search functions
    Refactor sample and search functions for consistency.

--- sample_search.py
+++ sample_search.py
@@ -1,8 +1,9 @@
-def sample(nums): # this function is used to ask the model to generate the commits like, it works well
-  for i in range(nums):
+def sample(nums):
+  for i in range(nums): 
     out = []
     context = [0] * block_size
-    while True:
+    while True: 
+      
       contx  = torch.tensor([context])
       logits = model(contx)
       probs  = F.softmax(logits, dim=-1)
@@ -16,12 +17,12 @@ def sample(nums): # this function is used to ask the model to generate the commi
     print(''.join(out))
     print("") # print empty string to have spacing when the model sample nums > 1
     
-# to search whether the model is getting some words type from the data or it's just making them up
 def search(x: str):
   count= 0
   cont = []
   
-  for c in commits:
+  for c in commits: 
+    
     if x.lower() in c.lower():
       cont.append(c)
       count+=1
